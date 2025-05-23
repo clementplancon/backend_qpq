425dcef33f03ac35a9545b658fac780aee0936d1
clementplancon <clement.plancon.pro@gmail.com>
2025-05-23 13:07:02 +0200

    init

---
+++ index.js
@@ -0,0 +1,65 @@
+import dotenv from 'dotenv';
+import express, { json } from 'express';
+import cors from 'cors';
+import { Mistral } from "@mistralai/mistralai";
+
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+const APP_API_KEY = process.env.APP_API_KEY;
+const MISTRAL_API_KEY = process.env.MISTRAL_API_KEY;
+
+const client = new Mistral({
+    apiKey: MISTRAL_API_KEY
+});
+
+app.use(cors());
+app.use(json({limit: '50mb'})); // Increase the limit to 50mb
+
+app.use((req, res, next) => {
+    const apiKey = req.headers['x-api-key'];
+    if (apiKey !== APP_API_KEY) {
+        return res.status(403).json({ error: 'Forbidden' });
+    }
+    next();
+});
+
+app.post('/api/ticket-mistral-ocr', async (req, res) => {
+    try {
+        const { base64_image } = req.body;
+        if (!base64_image) {
+            return res.status(400).json({ error: 'Scan manquant. Veuillez scanner un document avant d\'en extraire des informations.' });
+        }
+
+        const mistralBody = {
+            model: "mistral-small-latest",
+            messages: [
+                {
+                    role: "user",
+                    content: [
+                        {
+                            type: "text",
+                            text: "Voici un scan d'un ticket de caisse. Je veux que tu extraies la listes des articles et leur prix unitaire. Si tu vois deux fois le même article sur une même ligne avec un prix total et non un prix unitaire, je veux que tu les sépares en deux lignes. Je veux que tu me renvoies un JSON avec une liste d'objets contenant le nom de l'article et son prix unitaire. Le format du JSON : {'articles': [{'nomArticle': nom_article, 'prixUnitaire': prix_unitaire}, ...]}. Si l'image est floue, illisible ou que ce n'est pas un ticket de caisse, je veux que tu me renvoies l'objet JSON suivant : { 'error': 'cannot_read' }. La réponse doit être un objet JSON et rien d'autre. Sans texte réalable, sans formatage, sans retour à la ligne, sans retour chariot de type '\\n' juste le JSON brut sans rien d'autre.",
+                        },
+                        {
+                            type: "image_url",
+                            imageUrl: "data:image/jpeg;base64," + base64_image,
+                        },
+                    ],
+                },
+            ],
+        };
+
+        const mistralResponse = await client.chat.complete(mistralBody);
+
+        res.json(JSON.parse(mistralResponse.choices[0].message.content));
+    } catch (err) {
+        res.status(err.response?.status || 500).json({ error: err.message, details: err.response?.data });
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
